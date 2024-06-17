891b7dde79e222b10b0030b44b9cef42a2fe74f8
Vipin Chandran Muthirikkaparambil <73341926+mvipinchand@users.noreply.github.com>
2024-06-16 21:38:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import os
 current_time = datetime.datetime.now()
 
 # Define the file path on your desktop
-desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-file_path = os.path.join(desktop_path, 'current_time.txt')
+file_path = "C:\Users\mvipi\Desktop\new 2.txt"
 
 # Write current time to the file
 with open(file_path, 'w') as file:
